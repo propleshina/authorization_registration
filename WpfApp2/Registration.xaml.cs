3fd5318e9d507a56e5b465b8cea7dbfe4cfa67b2
Мария Хитринович <217097@edu.fa.ru>
2024-04-05 11:41:20 +0300

    ghhj

--- WpfApp2/Registration.xaml.cs
+++ WpfApp2/Registration.xaml.cs
@@ -23,6 +23,18 @@ namespace WpfApp2
         public Registration()
         {
             InitializeComponent();
+            RoleComboBox.ItemsSource = new Roles[]
+            {
+                new Roles { Name = "Менеджер А"},
+                new Roles { Name = "Менеджер С"},
+                new Roles { Name = "Администратор"},
+            };
+        }
+
+        public class Roles
+        {
+            public string Name { get; set; } = "";
+            public override string ToString() => $"{Name }";
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
@@ -30,5 +42,30 @@ namespace WpfApp2
             NavigationService.Navigate(new AuthPage());
         }
 
+        private void RegButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(FIOtext.Text) && !string.IsNullOrEmpty(LoginText.Text) 
+                && !string.IsNullOrEmpty(PasswordText.Password)&& !string.IsNullOrEmpty(PasswordText.Password)
+                && !string.IsNullOrEmpty(TelephoneText.Text) && !string.IsNullOrEmpty(PhotoLinkText.Text) &&
+                (MaleRadioButton.IsChecked==true || FemaleRadioButton.IsChecked==true) && !(RoleComboBox.SelectedIndex==-1))
+            {
+                for (int i = 0; i < 11; i++)
+                {
+
+                }
+                if ()
+                {
+
+                }
+                else
+                {
+                    MessageBox.Show("Вы ввели телефон неправильно!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Вы заполнили не все поля :(");
+            }
+        }
     }
 }
